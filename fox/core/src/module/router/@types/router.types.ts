18397e6f315fadb1ffa0a725302363bf1ef87ab1
igorrm23 <igormachadofire14@gmail.com>
2026-04-21 17:25:14 -0300

    feat: add page unmount lifecycle support and internal link interception to router

--- fox/core/src/module/router/@types/router.types.ts
+++ fox/core/src/module/router/@types/router.types.ts
@@ -1,5 +1,6 @@
 export interface Page {
     mount(parent: HTMLElement): void;
+    unmount?(): void;
 }
 
 export type PageClass = new () => Page;
--- test_router.ts
+++ test_router.ts
@@ -5,10 +5,11 @@ const documentMock = {
         innerHTML: "",
         appendChild: () => { },
     }),
+    addEventListener: (type: string, listener: any) => { },
 };
 const windowMock = {
-    location: { pathname: "/" },
-    addEventListener: () => { },
+    location: { pathname: "/", origin: "http://localhost" },
+    addEventListener: (type: string, listener: any) => { },
 };
 const historyMock = {
     pushState: () => { },
@@ -18,27 +19,50 @@ const globalContext = globalThis as unknown as {
     document: typeof documentMock;
     window: typeof windowMock;
     history: typeof historyMock;
+    URL: typeof URL;
 };
 
 globalContext.document = documentMock;
 globalContext.window = windowMock;
 globalContext.history = historyMock;
+globalContext.URL = URL;
 
-import { FoxRouter, Page } from "./fox/core/src/module/router/router";
+import { FoxRouter } from "./fox/core/src/module/router/router";
+import { Page } from "./fox/core/src/module/router/@types/router.types";
+
+let unmountCalled = false;
 
 class MockPage implements Page {
     mount(parent: HTMLElement) {
         console.log("MockPage mounted!");
     }
+    unmount() {
+        console.log("MockPage unmounted!");
+        unmountCalled = true;
+    }
+}
+
+class AnotherPage implements Page {
+    mount(parent: HTMLElement) {
+        console.log("AnotherPage mounted!");
+    }
 }
 
 const routes = {
     "/": MockPage,
+    "/another": AnotherPage,
 };
 
 console.log("Initializing Router...");
 const router = new FoxRouter(routes);
 router.start();
 
-console.log("Navigating to /...");
-router.navigate("/");
+console.log("Navigating to /another...");
+router.navigate("/another");
+
+if (unmountCalled) {
+    console.log("Verification SUCCESS: unmount() was called!");
+} else {
+    throw new Error("Verification FAILURE: unmount() was NOT called!");
+}
+
