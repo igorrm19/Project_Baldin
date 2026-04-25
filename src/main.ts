846851f665ffa397e519a564d1f7bff9277211f0
igorrm23 <igormachadofire14@gmail.com>
2026-04-24 22:30:28 -0300

    refactor: reorganize login feature components and UI modules to improve project structure

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,8 @@ import { FoxRouter } from '../fox/core/src/module/router/router';
 import { MainPage } from './App/shared/pages/mainPage';
 import { AboutPage } from './App/shared/pages/aboutPage';
 import { test } from './convert.stringtoobject';
-import { html } from './App/shared/features/login/ui/cardLogin/card';
+import { html } from './App/shared/features/login/components/cardLogin/card';
 import { parseHTML } from '../fox/core/src/module/dom/parserDiv';
-
 
 
 import { actionStack } from '../fox/action.stack';
