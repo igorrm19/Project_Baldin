50845f2199a3d9f5a7d7de0fa88128e71d8f22b6
igorrm23 <igormachadofire14@gmail.com>
2026-04-24 22:38:54 -0300

    feat: implement CI workflow with Codecov integration and coverage thresholds

--- jest.config.js
+++ jest.config.js
@@ -13,6 +13,14 @@ export default {
     ],
     coverageDirectory: 'coverage',
     coverageReporters: ['text', 'lcov'],
+    coverageThreshold: {
+        global: {
+            branches: 85,
+            functions: 85,
+            lines: 85,
+            statements: 85
+        }
+    },
     coveragePathIgnorePatterns: ['/fox/test/', '/fox/core/src/@types/'],
     transform: {
         '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: 'tsconfig.jest.json' }]
