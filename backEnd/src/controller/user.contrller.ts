17901968384cadc9ccd89d5020425bc54e660a91
igorrm23 <igormachadofire14@gmail.com>
2026-05-10 02:48:45 -0300

    .

---
+++ backEnd/src/controller/user.contrller.ts
@@ -0,0 +1,25 @@
+import type{ Request, Response } from 'express';
+
+export const getUsers = async (req: Request, res: Response) => {
+    //
+}
+
+export const UserById = async (req: Request, res: Response) => {
+    //
+}
+
+export const createUser = async (req: Request, res: Response) => {
+    //
+}
+
+export const updateUser = async (req: Request, res: Response) => {
+    //
+}
+
+export const deleteUser = async (req: Request, res: Response) => {
+    //  
+}
+
+export const login = async (req: Request, res: Response) => {
+    //
+}
--- backEnd/src/routes/user.router.ts
+++ backEnd/src/routes/user.router.ts
@@ -1,9 +1,17 @@
 import express from 'express';
+import { getUsers, UserById, createUser, updateUser, deleteUser, login } from '../controller/user.contrller.js';
 
 const router = express.Router();
 
 router.get(['/', '/api'], (req, res) => {
     res.send('Hello World!');
 });
+
+router.get('/users', getUsers);
+router.get('/users/:id', UserById);
+router.post('/users', createUser);
+router.put('/users/:id', updateUser);
+router.delete('/users/:id', deleteUser);
+router.post('/login', login);
 
 export default router;
