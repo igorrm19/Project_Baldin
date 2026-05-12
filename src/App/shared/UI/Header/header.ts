fbd9015be181cdd0b521cf29ee1133974d77d702
igorrm23 <igormachadofire14@gmail.com>
2026-05-12 12:21:18 -0300

    refactor: update logo path, add button type, and streamline component mounting in HeaderComponent

--- src/App/shared/UI/Header/header.ts
+++ src/App/shared/UI/Header/header.ts
@@ -2,24 +2,17 @@ import { BaseModel } from "../../../../../fox/core/src/module/utils/base.model"
 import template from "./header.html?raw"
 
 export class HeaderComponent extends BaseModel {
-    containerHeader: HTMLElement;
 
     constructor() {
         const rawTemplate = template as string | { default: string };
         const finalTemplate = typeof rawTemplate === 'string' ? rawTemplate : (rawTemplate?.default || "<header></header>");
-        super("header", finalTemplate);
-        this.containerHeader = document.createElement("div");
+        super("div", finalTemplate);
         return;
     }
 
     override mount(parent: HTMLElement): void {
-
-        const html = this.getHTML()
-        // eslint-disable-next-line no-unsanitized/property
-        this.containerHeader.innerHTML = html
-        this.onMount()
-
-        parent.appendChild(this.containerHeader)
+        super.mount(parent);
+        this.onMount();
         return;
     }
 
